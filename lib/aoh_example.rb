617ebe745b29abf5304ab29dd399b10f65f104ea
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-21 11:30:33 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,7 +7,9 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  new_array = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  new_array = [{ name:  "Don Gately", occupation: "Live-in Staff" }, { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" }, { name:  "Kate Gompert", occupation: "None" },
+   { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
 
 def literal_aoh
